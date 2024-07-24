b9f5ff8c7cd39c3bc9a2583b3bc36753931797ab
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-07-24 13:36:31 +0200

    Add aws api gateway url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://789tp00z47.execute-api.eu-north-1.amazonaws.com/dev',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
