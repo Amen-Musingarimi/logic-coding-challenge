0e3d6a67c5b4aac7caf79962ba6952d92f72ff70
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2024-07-24 13:05:32 +0200

    Setup backend function on lambda

---
+++ amplify/backend/function/honoProxyFunction/src/app.js
@@ -0,0 +1,60 @@
+import { Hono } from 'hono';
+import jwt from 'jsonwebtoken';
+import { serveStatic } from 'hono/bun';
+
+const app = new Hono();
+
+app.use('*', serveStatic({ root: './dist' }));
+
+app.post('/login', async (c) => {
+  const { username, password } = await c.req.json();
+
+  if (!username || username.trim() === '') {
+    return c.json({ message: 'Username required' }, 400);
+  }
+  if (!password || password.trim() === '') {
+    return c.json({ message: 'Password required' }, 400);
+  }
+
+  try {
+    const res = await fetch(
+      'https://7qk9m2xvu2.us-west-2.awsapprunner.com/v1/auth/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-tenantid': 'SchryverPruebas',
+        },
+        body: JSON.stringify({ username, password }),
+      }
+    );
+
+    if (!res.ok) {
+      const errorData = await res.json();
+      return c.json(
+        { message: errorData.message || 'Invalid username or password' },
+        res.status
+      );
+    }
+
+    const data = await res.json();
+    const token = data.access_token;
+
+    if (!token) {
+      throw new Error('No token received from the authentication server');
+    }
+
+    const userData = JSON.stringify(jwt.decode(token), null, 2);
+
+    if (!userData) {
+      throw new Error('Failed to decode the token');
+    }
+
+    return c.json({ message: 'Login success', user: userData }, 200);
+  } catch (error) {
+    console.log(error.message);
+    return c.json({ message: 'Internal Server Error' }, 500);
+  }
+});
+
+export default app;
